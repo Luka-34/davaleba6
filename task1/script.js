0ea9c18f30001f646871056d55bbfcf8e903930f
Luka-34 <lchikhladze960@gmail.com>
2024-05-31 22:12:46 +0400

    initial commit

---
+++ task1/script.js
@@ -0,0 +1,6 @@
+// შექმენი button, რომელზე დაწკაპების
+// შემდეგაც გაიხსნება მოდალი და მოდალის
+// უკან შავი/გამჭვირვალე background
+
+const button = document.createElement('button');
+button.className = 'btn';
