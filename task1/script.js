724dcb5266a367a8ba755d0c7e1e2e88b312dd53
Luka-34 <lchikhladze960@gmail.com>
2024-06-02 15:23:07 +0400

    initial commit

--- task1/script.js
+++ task1/script.js
@@ -2,5 +2,23 @@
 // შემდეგაც გაიხსნება მოდალი და მოდალის
 // უკან შავი/გამჭვირვალე background
 
-const button = document.createElement('button');
-button.className = 'btn';
+const modal = document.createElement('div');
+const button = document.querySelector('.btn');
+const backBtn = document.createElement('button');
+
+modal.className = 'modal';
+document.body.appendChild(modal);
+
+
+function Modal() {
+   document.body.style.backgroundColor = 'black';
+   document.body.style.display = 'flex';
+   document.body.style.justifyContent = 'center';
+   document.body.style.alignItems = 'center';
+}
+
+button.addEventListener('click', () => {
+    Modal()
+})
+
+
